aca261afd3f8da50fcc58803488b7927a2f55a35
Kien Ngo <kienn@aareas.com>
2022-05-20 05:29:11 -0500

    Dynamic load for SortContainer

--- components/CardDialog.tsx
+++ components/CardDialog.tsx
@@ -50,12 +50,6 @@ const CardDialog = ({ cardDialogOpen, setCardDialogOpen, selectedCardForDialog }
                                 ))
                             }
                         </ul>
-                        <button className="absolute bottom-1 rounded-full focus:outline-none 
-                        text-white bg-purple-700 hover:bg-purple-800 focus:ring-purple-300 
-                        font-medium text-sm px-5 py-2.5 mb-2 dark:bg-purple-600
-                        dark:hover:bg-purple-700 dark:focus:ring-purple-900">
-                            Learn more
-                        </button>
                     </Dialog.Panel>
                 </div>
             </div>
--- components/SearchSection/SearchSection.tsx
+++ components/SearchSection/SearchSection.tsx
@@ -11,12 +11,13 @@ import { Card } from '../../classes/card.model'
 const SearchSectionMobileViewDynamic = dynamic(() => import('./SearchSectionMobileView'));
 const FilteredCardsContainerDynamic = dynamic(() => import("../FilteredCardsContainer"));
 const CardDialogDynamic = dynamic(() => import("../CardDialog"));
+const SortContainerDynamic = dynamic(() => import('../SortContainer'));
 
 const SearchSection = () => {
     const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
     const [cardDialogOpen, setCardDialogOpen] = useState(false);
     const [selectedCardForDialog, setSelectedCardForDialog] = useState<Card|any>({});
-    const { updateFilterOptions, sortOptions, updateSortOptions, filteredCards } = useContext(SearchSectionFilterContext);
+    const { updateFilterOptions, filteredCards } = useContext(SearchSectionFilterContext);
     return (
         <>
             {/* Mobile filter dialog */}
@@ -45,37 +46,7 @@ const SearchSection = () => {
                                     />
                                 </Menu.Button>
                             </div>
-
-                            <Transition
-                                as={Fragment}
-                                enter="transition ease-out duration-100"
-                                enterFrom="transform opacity-0 scale-95"
-                                enterTo="transform opacity-100 scale-100"
-                                leave="transition ease-in duration-75"
-                                leaveFrom="transform opacity-100 scale-100"
-                                leaveTo="transform opacity-0 scale-95"
-                            >
-                                <Menu.Items className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-2xl bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                    <div className="py-1">
-                                        {sortOptions.map((option) => (
-                                            <Menu.Item key={option.name}>
-                                                {({ active }) => (
-                                                    <a
-                                                        href={option.href}
-                                                        className={mergeTailwindClass(
-                                                            option.current ? 'font-medium text-gray-900' : 'text-gray-500',
-                                                            active ? 'bg-gray-100' : '',
-                                                            'block px-4 py-2 text-sm'
-                                                        )}
-                                                    >
-                                                        {option.name}
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                        ))}
-                                    </div>
-                                </Menu.Items>
-                            </Transition>
+                            <SortContainerDynamic></SortContainerDynamic>
                         </Menu>
 
                         <button type="button" className="p-2 -m-2 ml-5 sm:ml-7 text-gray-400 hover:text-gray-500">
